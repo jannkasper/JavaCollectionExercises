d378141d5ae621ef49a7f65615d572f8a00d32cf
Jan K <jann.kasper@gmail.com>
2019-05-16 07:11:54 +0200

    PalindromeLinkedList done

--- src/collectionExercises/PalindromeLinkedList.java
+++ src/collectionExercises/PalindromeLinkedList.java
@@ -1,13 +1,37 @@
 package collectionExercises;
 
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.LinkedList;
 
 public class PalindromeLinkedList {
     public boolean palindromeList (Integer[] array) {
+        if(array == null) return  false;
+
+        boolean check;
         LinkedList<Integer> list = new LinkedList<>(Arrays.asList(array));
 
-        return true;
+        check = pollMethod(list);
 
+        check = recursionMethod(list.iterator(), list.descendingIterator());
+
+        return check;
     }
+
+    public boolean pollMethod(LinkedList linkedList){
+        while (linkedList.size() != 0) {
+            if(linkedList.size() == 1) return true;
+            if (linkedList.pollFirst() != linkedList.pollLast()) return false;
+        }
+        return true;
+    }
+
+    public boolean recursionMethod(Iterator front, Iterator back){
+        if (!front.hasNext() && !back.hasNext()) return true;
+
+        if (front.next() == back.next()) return recursionMethod(front,back);
+
+        return false;
+    }
+
 }
